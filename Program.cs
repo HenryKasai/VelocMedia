83aad5f0b2497c38d02f743e6923b6892baddbec
HenryKasai <henrykasai1775@gmail.com>
2020-04-30 20:21:39 -0300

    Exercício resolvido.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace VelocMedia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Insira os dados de seu objeto em estudo, e veja sua velocidade média.");
+
+            Console.Write("Distância percorrida em metros:");
+            string d = Console.ReadLine();
+            decimal Distância = Convert.ToDecimal(d);
+
+            Console.Write("Tempo gasto em segundos:");
+            string t = Console.ReadLine();
+            decimal Tempo = Convert.ToDecimal(t);
+            
+            decimal resultfinal = Distância / Tempo;
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("A velocidade média de seu objeto em estudo é " + resultfinal + " m/s");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # VelocMedia
 Programa que calcula a velocidade média de determinado objeto.
+
+
+
+[console: VelocMedia] Calcule a velocidade, a partir da distância (Δd, em metros) e do tempo (Δt, em segundos).
+
+v = Δd / Δt
+
+Distância percorrida (m): 100
+Tempo gasto (s): 15
+
+Velocidade média: 6 m/s
